ab835ef8fbfb5fbaa25d46482ed729f2ea7efeba
Mike Nolta <mike@nolta.net>
2025-06-05 13:59:41 -0400

    add tidewave AI MCP agent for dev

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,6 @@
 FROM elixir:1.18-otp-27
 
 RUN apt-get update && apt-get install -y inotify-tools && rm -rf /var/lib/apt/lists/*
+
+# install Tidewave AI mcp-proxy
+RUN cd /usr/local/bin && curl -sL https://github.com/tidewave-ai/mcp_proxy_rust/releases/latest/download/mcp-proxy-x86_64-unknown-linux-musl.tar.gz | tar xvz
--- app/lib/myscinet_web/endpoint.ex
+++ app/lib/myscinet_web/endpoint.ex
@@ -26,6 +26,11 @@ defmodule MySciNetWeb.Endpoint do
     gzip: not code_reloading?,
     only: MySciNetWeb.static_paths()
 
+  # tidewave AI agent (dev only)
+  if Code.ensure_loaded?(Tidewave) do
+    plug Tidewave
+  end
+
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
   if code_reloading? do
