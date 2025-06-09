38471d9f152ad6713308cfd6ad69e0bc6bf827a1
Mike Nolta <mike@nolta.net>
2025-06-09 10:49:17 -0400

    SetLocale: use configured default locale

--- app/lib/myscinet_web/plugs/set_locale.ex
+++ app/lib/myscinet_web/plugs/set_locale.ex
@@ -4,14 +4,15 @@ defmodule MySciNetWeb.Plugs.SetLocale do
   def init(opts), do: opts
 
   def call(conn, _opts) do
-    accept_language = get_req_header(conn, "accept-language") |> List.first() || "en"
+    default_locale = Application.get_env(:myscinet, MySciNetWeb.Gettext)[:default_locale] || "en"
+    accept_language = get_req_header(conn, "accept-language") |> List.first() || default_locale
     available_locales = Gettext.known_locales(MySciNetWeb.Gettext)
     locale =
       accept_language
       |> String.split(",")
       |> Enum.map(&String.trim/1)
       |> Enum.map(&String.slice(&1, 0, 2))
-      |> Enum.find(fn lang -> lang in available_locales end) || "en"
+      |> Enum.find(fn lang -> lang in available_locales end) || default_locale
 
     IO.puts("Setting locale to: #{locale}")
     Gettext.put_locale(locale)
