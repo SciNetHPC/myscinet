da6657f2caa6e05c8240c8345abf6979157cf403
Mike Nolta <mike@nolta.net>
2025-06-10 11:25:14 -0400

    job: add script & env

--- app/lib/myscinet_web/controllers/job_controller.ex
+++ app/lib/myscinet_web/controllers/job_controller.ex
@@ -2,7 +2,7 @@ defmodule MySciNetWeb.JobController do
   use MySciNetWeb, :controller
 
   alias MySciNet.Repo
-  alias MySciNet.Tcjsum
+  alias MySciNet.Tgjsum
   import Ecto.Query
 
   @page_size 20
@@ -12,7 +12,7 @@ defmodule MySciNetWeb.JobController do
     offset = (page - 1) * @page_size
 
     jobs =
-      Tcjsum
+      Tgjsum
       |> order_by([j], desc: j.submit)
       |> limit(^@page_size)
       |> offset(^offset)
@@ -22,7 +22,16 @@ defmodule MySciNetWeb.JobController do
   end
 
   def show(conn, %{"id" => id}) do
-    job = Repo.get_by!(Tcjsum, jobid: id)
-    render(conn, "show.html", job: job)
+    job = Repo.get_by!(Tgjsum, jobid: id)
+    script = Repo.get_by(MySciNet.Tgjscript, jobid: id)
+    command_row = Repo.get_by(MySciNet.Tgjcom, jobid: id)
+    command =
+      case command_row && command_row.jobcom do
+        nil -> nil
+        str -> String.replace(str, "/opt/slurm/bin/sbatch --export=NONE", "sbatch")
+      end
+    env_row = Repo.get_by(MySciNet.Tgjenv, jobid: id)
+    jobenv = env_row && env_row.jobenv
+    render(conn, "show.html", job: job, jobscript: script && script.jobscript, command: command, jobenv: jobenv)
   end
 end
