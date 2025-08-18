f3de1ba65bf3e5f67f83bfebea2876d8c6d36e1e
Mike Nolta <mike@nolta.net>
2025-08-18 14:15:00 -0400

    v0.8.1

--- app/mix.exs
+++ app/mix.exs
@@ -4,7 +4,7 @@ defmodule MySciNet.MixProject do
   def project do
     [
       app: :myscinet,
-      version: "0.8.0",
+      version: "0.8.1",
       elixir: "~> 1.15",
       elixirc_paths: elixirc_paths(Mix.env()),
       erlc_paths: elixirc_paths(Mix.env()),
