4aae22e73e6caf5c6be7ae3941fc4c6da5f774a5
Mike Nolta <mike@nolta.net>
2025-08-26 09:43:43 -0400

    user(s): add page titles

--- app/lib/myscinet_web/controllers/user_controller.ex
+++ app/lib/myscinet_web/controllers/user_controller.ex
@@ -4,7 +4,7 @@ defmodule MySciNetWeb.UserController do
   def index(conn, %{"q" => q}) do
     case MySciNet.LDAP.user_search(q) do
       {:ok, users} ->
-        render(conn, :index, users: users)
+        render(conn, :index, page_title: "users #{q}", users: users)
 
       error ->
         dbg(error)
@@ -15,12 +15,12 @@ defmodule MySciNetWeb.UserController do
     end
   end
 
-  def index(conn, _), do: render(conn, :index)
+  def index(conn, _), do: render(conn, :index, page_title: "users")
 
   def show(conn, %{"id" => id}) do
     case MySciNet.LDAP.user_info(id) do
       {:ok, info} ->
-        render(conn, :show, user: info)
+        render(conn, :show, page_title: "user #{info.username}", user: info)
 
       error ->
         dbg(error)
