99fc784d5ce4ba8174e0c74d5214504e78d1f7eb
Mike Nolta <mike@nolta.net>
2025-11-10 11:27:01 -0500

    job: add gpu power plot

--- app/lib/myscinet_web/controllers/job_controller.ex
+++ app/lib/myscinet_web/controllers/job_controller.ex
@@ -274,6 +274,7 @@ defmodule MySciNetWeb.JobController do
            :memfree,
            :buffers,
            :cached,
+           :dcgm_fi_dev_power_usage,
            :dcgm_fi_prof_gr_engine_active,
            :dcgm_fi_prof_pipe_fp16_active,
            :dcgm_fi_prof_pipe_fp32_active,
