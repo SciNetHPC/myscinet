5ed9f5519b0f9a43559b0a6e0a6c630c1ca21b79
Mike Nolta <mike@nolta.net>
2026-05-04 11:48:29 -0400

    allocation: nicer rounding for gpus

--- app/lib/myscinet_web/controllers/allocation_html.ex
+++ app/lib/myscinet_web/controllers/allocation_html.ex
@@ -1,4 +1,12 @@
 defmodule MySciNetWeb.AllocationHTML do
   use MySciNetWeb, :html
   embed_templates "allocation_html/*"
+
+  def nice_round_cores(x) do
+    case x do
+      x when x >= 100 -> round(x)
+      x when x >= 10 -> Float.round(x, 1)
+      _ -> Float.round(x, 2)
+    end
+  end
 end
